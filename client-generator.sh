159d59571ce4bcabc4099bc6e401be78329e20f0
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-07-17 15:04:11 +0530

    updated config.yml

--- client-generator.sh
+++ client-generator.sh
@@ -4,7 +4,7 @@
 SWAGGER_JAR="swagger-codegen-cli.jar"
 INPUT_SPEC_FILE="https://gist.githubusercontent.com/Grandolf49/62b084ebeec49ae56f729466d0c5ceb0/raw/bc63b70f0c6298a1e99505ebc3445f7f3b6b945d/response.json"
 CLIENT_LANGUAGE="java"
-OUTPUT_FOLDER="java/retrofit2"
+OUTPUT_FOLDER="out"
 CONFIG_FILE_NAME="config.json"
 
 echo "Generating Fineract Client SDK..."
